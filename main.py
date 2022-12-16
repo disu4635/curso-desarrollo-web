653568fffe0917c8741cb2698a86dd963f62a9b4
David Alejandro Espinosa <davidespinosa4635@gmail.com>
2022-12-15 23:03:38 -0500

    Validacion en Beneficio y punto de entrada

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ class Usuario():
         self.cedula = cedula
         self.edad = edad
 
+
 class Cuenta (Usuario):
     monto = 0
 
@@ -18,35 +19,32 @@ class Cuenta (Usuario):
         self.monto = monto
 
     def mostrar(self):
-        print(f"La cuenta de {self.nombre} {self.apellido}, de {self.edad} años, con cedula {self.cedula}, tiene un monto de {self.monto} pesos")
-
-    def beneficiar(self):
-        if(self.edad >= 18 and self.edad <=28):
-            return True
-        return False
-        
+        print(
+            f"La cuenta de {self.nombre} {self.apellido}, de {self.edad} años, con cedula {self.cedula}, tiene un monto de {self.monto} pesos")
 
     def ingresar(self, dinero):
-        if(dinero < 0):
+        if (dinero < 0):
             print('No se admiten valores negativos')
             return False
-        
+
         self.monto += dinero
-        print(f'Se ingresaron {dinero} pesos, el valor actual de la cuenta es {self.monto}')
+        print(
+            f'Se ingresaron {dinero} pesos, el valor actual de la cuenta es {self.monto}')
         return True
 
     def retirar(self, dinero):
-        if(dinero < 0):
+        if (dinero < 0):
             print('No se admiten valores negativos')
             return False
-        
+
         self.monto -= dinero
-        print(f'Se retiraron {dinero} pesos, el valor actual de la cuenta es {self.monto}')
+        print(
+            f'Se retiraron {dinero} pesos, el valor actual de la cuenta es {self.monto}')
         return True
 
     def getNombre(self):
         return self.nombre
-    
+
     def getApellido(self):
         return self.apellido
 
@@ -58,10 +56,10 @@ class Cuenta (Usuario):
 
     def getMonto(self):
         return self.monto
-    
+
     def setNombre(self, nombre):
         self.nombre = nombre
-    
+
     def setApellido(self, apellido):
         self.apellido = apellido
 
@@ -74,20 +72,29 @@ class Cuenta (Usuario):
     def setMonto(self, monto):
         self.monto = monto
 
-class beneficio(Cuenta):
+
+class Beneficio(Cuenta):
     def __init__(self, nombre, apellido, cedula, edad, monto):
         super().__init__(nombre, apellido, cedula, edad, monto)
         self.monto += self.monto*0.05
 
-usuario_1 = Usuario('Juan', 'Perez', 123, 20)
-cuenta_1 = Cuenta(usuario_1.nombre, usuario_1.apellido, usuario_1.cedula, usuario_1.edad, 10000)
+    def validar(self):
+        return self.edad >= 18 and self.edad <= 28
 
-cuenta_1.mostrar()
-cuenta_1.ingresar(10000)
-cuenta_1.retirar(5000)
+# Ejemplo de uso
 
-if(cuenta_1.beneficiar()):
-    ben1 = beneficio(cuenta_1.nombre, cuenta_1.apellido, cuenta_1.cedula, cuenta_1.edad, cuenta_1.monto)
-    ben1.mostrar()
-else: 
-    print('Esta cuenta no es apta para beneficios')
+if __name__ == '__main__':
+    usuario_1 = Usuario('Juan', 'Perez', 123, 20)
+    cuenta_1 = Cuenta(usuario_1.nombre, usuario_1.apellido,
+                    usuario_1.cedula, usuario_1.edad, 10000)
+
+    cuenta_1.mostrar()
+    cuenta_1.ingresar(10000)
+    cuenta_1.retirar(5000)
+
+    ben1 = Beneficio(cuenta_1.nombre, cuenta_1.apellido,
+                    cuenta_1.cedula, cuenta_1.edad, cuenta_1.monto)
+    if (ben1.validar()):
+        ben1.mostrar()
+    else:
+        print('Esta cuenta no es apta para beneficios')
